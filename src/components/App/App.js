1ddb2c5b736751733cb3c7c22b395d1d16f22bb2
Alexander Miliutin <alexander.milutin@csssr.com>
2018-02-12 03:21:38 +0200

    Реорганизация структуры файлов, подключение bootstrap

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
