273257d88b0ee9a8b11f281b531c8ce7e74524dc
Alexander Miliutin <alexander.milutin@csssr.com>
2018-02-12 03:38:31 +0200

    Подключение роутера, создание пустых страниц Main и ContactsSearch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
 import React, { PureComponent } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import ContactsSearch from '../../pages/ContactsSearch/ContactsSearch';
+import Main from '../../pages/Main/Main';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Main} />
+          <Route path='/contacts-search/' component={ContactsSearch} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/ContactsSearch/ContactsSearch.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class ContactsSearch extends PureComponent {
+    render() {
+        return (
+            <div>
+                contactsSearch
+            </div>
+        );
+    }
+}
